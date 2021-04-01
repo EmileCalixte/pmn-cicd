3bec816a69c64a2b070e23ad00ca293e19c58d5f
Emile Calixte <calixte.emile@gmail.com>
2021-04-01 12:41:53 +0200

    Alo

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+"""This module does blah blah."""
+
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """This function does blah blah."""
     return "Hello world !"
 
 
